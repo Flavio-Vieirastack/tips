4d2480b53d437d8a4a5c0e88353861ed8fe26b72
Flavio Vieira <flavioextreme1@hotmail.com>
2024-04-23 19:28:31 -0300

    adicionado projeto

---
+++ resumed-model/src/main/java/com/resumedmodel/resumedmodel/ResumedModelApplication.java
@@ -0,0 +1,13 @@
+package com.resumedmodel.resumedmodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumedModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumedModelApplication.class, args);
+	}
+
+}
---
+++ resumed-model/src/test/java/com/resumedmodel/resumedmodel/ResumedModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resumedmodel.resumedmodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumedModelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
