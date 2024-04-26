e267a88e97e40290a26ee21459cb4e2ec3ff9fb4
Flavio Vieira <flavioextreme1@hotmail.com>
2024-04-26 13:03:35 -0300

    commit inicial

---
+++ strategy-factory/src/test/java/com/strategyandfactory/strategyfactory/StrategyFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.strategyandfactory.strategyfactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrategyFactoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
