89b2222e2c1e9d779ce06467cf9889860ffb7ede
Flavio Vieira <flavioextreme1@hotmail.com>
2024-05-03 13:51:55 -0300

    commit inicial

---
+++ initBinder/src/test/java/com/initbinder/initBinder/InitBinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.initbinder.initBinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitBinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
