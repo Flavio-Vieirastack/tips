edb8469183b983b2bd79debad7db27d44362c2af
Flavio Vieira <flavioextreme1@hotmail.com>
2024-05-05 17:00:45 -0300

    terminado aula do conditional

---
+++ initBinder/src/main/java/com/initbinder/initBinder/Condition/MyBean.java
@@ -0,0 +1,7 @@
+package com.initbinder.initBinder.Condition;
+
+public class MyBean {
+    public MyBean() {
+        System.out.println("Bean criado");
+    }
+}
---
+++ initBinder/src/main/java/com/initbinder/initBinder/Condition/MyConfig.java
@@ -0,0 +1,14 @@
+package com.initbinder.initBinder.Condition;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyConfig {
+    @Bean
+    @Conditional(MyCondition.class)
+    public MyBean bean() {
+        return new MyBean();
+    }
+}
